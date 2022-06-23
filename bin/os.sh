01aa2de3251f251e5d41dfea972e6ed7d5a72763
Loyxin <loy_xin@163.com>
2022-06-23 23:15:42 +0800

    alacritty compton

--- bin/os.sh
+++ bin/os.sh
@@ -14,4 +14,15 @@ if [[ "$system_type" == "Linux" ]]; then
 	if [[ -f ~/.tmux/plugins/tpm/tpm ]]; then
 		git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 	fi
+
+ln -s ~/bin/conf/alacritty.yml ~/.config/alacritty.yml
+ln -s ~/bin/conf/compton.conf ~/.config/compton.conf
+ln -s ~/bin/conf/neofetch ~/.config/neofetch
+ln -s ~/bin/conf/ranger ~/.config/ranger
+ln -s ~/bin/conf/polybar ~/.config/polybar
+ln -s ~/bin/conf/i3 ~/.config/i3
+ln -s ~/bin/conf/background ~/.config/.background
+ln -s ~/bin/conf/conkyrc ~/.conkyrc
+
 fi
+
